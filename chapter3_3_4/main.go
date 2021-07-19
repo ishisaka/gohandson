be2499b8845ca798b76df2e8e14bff6b808b9e4a
Tadahiro Ishisaka <ishisaka@gmail.com>
2021-07-19 13:56:25 +0900

    変更

--- chapter3_3_4/main.go
+++ chapter3_3_4/main.go
@@ -16,17 +16,24 @@ type GData interface {
 }
 
 // NData is structure
+// type NData struct {
+// 	Name string
+// 	Data int
+// }
 type NData struct {
 	Name string
-	Data int
+	Data []int
 }
 
 // Set is NData method
 func (nd *NData) Set(m string, g General) GData {
 	nd.Name = m
 	// nd.Data = g.(int)
-	if reflect.TypeOf(g).Kind() == reflect.Int {
-		nd.Data = g.(int)
+	// if reflect.TypeOf(g).Kind() == reflect.Int {
+	// 	nd.Data = g.(int)
+	// }
+	if reflect.TypeOf(g) == reflect.SliceOf(reflect.TypeOf(0)) {
+		nd.Data = g.([]int)
 	}
 	return nd
 }
@@ -39,15 +46,19 @@ func (nd *NData) Print() {
 //SData is Structure
 type SData struct {
 	Name string
-	Data string
+	// Data string
+	Data []string
 }
 
 // Set is SData method
 func (sd *SData) Set(m string, g General) GData {
 	sd.Name = m
 	// sd.Data = g.(string)
-	if reflect.TypeOf(g).Kind() == reflect.String {
-		sd.Data = g.(string)
+	// if reflect.TypeOf(g).Kind() == reflect.String {
+	// 	sd.Data = g.(string)
+	// }
+	if reflect.TypeOf(g) == reflect.SliceOf(reflect.TypeOf("")) {
+		sd.Data = g.([]string)
 	}
 	return sd
 }
@@ -59,10 +70,14 @@ func (sd *SData) Print() {
 
 func main() {
 	var data = []GData{}
-	data = append(data, new(NData).Set("Taro", 123))
-	data = append(data, new(SData).Set("Jiro", "Hello!"))
+	// data = append(data, new(NData).Set("Taro", 123))
+	// data = append(data, new(SData).Set("Jiro", "Hello!"))
+	// data = append(data, new(NData).Set("Hanako", 98700))
+	// data = append(data, new(SData).Set("Sachiko", []string{"happy?"}))
+	data = append(data, new(NData).Set("Taro", []int{1, 2, 3}))
+	data = append(data, new(SData).Set("Jiro", []string{"hello", "bye"}))
 	data = append(data, new(NData).Set("Hanako", 98700))
-	data = append(data, new(SData).Set("Sachiko", []string{"happy?"}))
+	data = append(data, new(SData).Set("Sachiko", "happy?"))
 	for _, ob := range data {
 		ob.Print()
 	}
